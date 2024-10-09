6fc2f1a141eeec9c38515635bac3e5935d7b5408
Altbergz <eric.altberg.acc@gmail.com>
2024-10-10 00:10:23 +0300

    add GitHub integration configuration and external service enum

---
+++ app/Enums/ExternalServiceEnum.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+enum ExternalServiceEnum : string
+{
+    case GITHUB = 'github';
+}
--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         ],
     ],
 
+    "github" => [
+        "client_id" => env("GITHUB_CLIENT_ID"),
+        "client_secret" => env("GITHUB_CLIENT_SECRET"),
+        "redirect" => env("GITHUB_REDIRECT_URI"),
+        "github_api_url" => env("GITHUB_API_URL"),
+    ],
 ];
