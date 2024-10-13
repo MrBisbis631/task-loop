f012492a62a7744726b01300c807bc7d26938e1c
Altbergz <eric.altberg.acc@gmail.com>
2024-10-13 23:26:53 +0300

    add TypeScript attributes and update ExternApiDetail model properties

--- app/Models/ExternApiDetail.php
+++ app/Models/ExternApiDetail.php
@@ -5,12 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\TypeScriptTransformer\Attributes\LiteralTypeScriptType;
+use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 
+#[TypeScript]
+#[LiteralTypeScriptType([
+    'id' => 'string',
+    'api_token' => 'string',
+    'api_secret' => 'string',
+    'api_name' => 'string',
+    'label' => 'string',
+    'description' => 'string',
+    'api_username' => 'string',
+    'expires_at' => 'string',
+    'created_at' => 'string',
+    'updated_at'=> 'string',
+    ])]
 class ExternApiDetail extends Model
 {
     use HasFactory;
 
     protected $fillable = ['expires_at', 'api_token', 'api_secret', 'api_name', 'label', 'description', 'api_username',];
+
+    protected $hidden = [ 'api_token', 'api_secret',];
 
     protected $casts = [
         'expires_at' => 'datetime',
