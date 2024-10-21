31eb4d636a6f34cc083fde832e7d1c26a6bda21b
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-10-22 01:37:24 +0300

    add Company index page component

---
+++ resources/js/Pages/FreelancerSpace/Company/Index.tsx
@@ -0,0 +1,12 @@
+import AppLayout from "@/Layouts/AppLayout";
+import React from "react";
+
+function Company() {
+  return (
+    <AppLayout title="Company">
+      <div>Company</div>
+    </AppLayout>
+  );
+}
+
+export default Company;
