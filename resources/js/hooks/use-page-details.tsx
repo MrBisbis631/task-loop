4bdab60f63e159c0a801c253c31adddf5b214b44
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-11-20 03:47:48 +0200

    feat: implement usePageDetails hook and define page details for freelancer, admin, client, and super-admin routes

---
+++ resources/js/hooks/use-page-details.tsx
@@ -0,0 +1,43 @@
+import useRoute from "@/Hooks/useRoute";
+import useTypedPage from "@/Hooks/useTypedPage";
+import freelancerRoutes from "@/page-details/freelancer";
+import adminRoutes from "@/page-details/admin";
+import clientRoutes from "@/page-details/client";
+import superAdminRoutes from "@/page-details/super-admin";
+import { route } from "ziggy-js";
+
+const pagesDetails = {
+  ...freelancerRoutes,
+  ...adminRoutes,
+  ...clientRoutes,
+  ...superAdminRoutes,
+};
+
+export function usePageDetails(): Page.PageDetails {
+  const { props } = useTypedPage();
+  const router = useRoute();
+  const currentRoute = pagesDetails[route().current() || ""];
+
+  if (!currentRoute) {
+    throw new Error(`No layout details found for route: ${route().current()}`);
+  }
+
+  const subRoute = currentRoute.getSubroute ? currentRoute.getSubroute(props) : undefined;
+  return {
+    head: currentRoute.head,
+    title: currentRoute.title(props),
+    description: currentRoute.description(props),
+    route: {
+      name: currentRoute.route.name,
+      url: router(currentRoute.route.routeName),
+    },
+    subRoute: subRoute ? { name: subRoute.name, url: router(...(subRoute.url as [string])) } : undefined,
+    pageItems: currentRoute.pageItems
+      ? currentRoute.pageItems(props).map(({ name, url, isActive }) => ({
+          title: name,
+          url: router(...(url as [string])),
+          isActive,
+        }))
+      : undefined,
+  };
+}
---
+++ resources/js/page-details/freelancer.tsx
@@ -0,0 +1,46 @@
+import { Separator } from "@/components/ui/separator";
+import { CompanyLinks, CompanyTags } from "@/Pages/FreelancerSpace/Company/CompaniesTable";
+import React from "react";
+
+export default {
+  "freelancer-space.external-api-details.index": {
+    head: "External API Details",
+    title: () => <span>External API Details</span>,
+    description: () => <span>Manage your external API details.</span>,
+    route: {
+      name: "External API Details",
+      routeName: "freelancer-space.external-api-details.index",
+    },
+    canAccess: user => user.role === "freelancer" || user.role === "admin" || user.role === "super-admin",
+  },
+  "freelancer-space.company.index": {
+    head: "Companies",
+    title: () => <span>Companies</span>,
+    description: () => <span>Manage your companies.</span>,
+    route: {
+      name: "Companies",
+      routeName: "freelancer-space.company.index",
+    },
+    canAccess: user => user.role === "freelancer" || user.role === "admin" || user.role === "super-admin",
+  },
+  "freelancer-space.company.show": {
+    head: "Company",
+    title: ({ company }) => <span>{company.name}</span>,
+    description: ({ company }) => (
+      <div className="flex gap-1">
+        <CompanyLinks company={company} />
+        <div className="pr-2">
+          <Separator orientation="vertical" className="" />
+        </div>
+        <CompanyTags company={company} />
+      </div>
+    ),
+    route: {
+      name: "Company",
+      routeName: "freelancer-space.company.index",
+    },
+    getSubroute: ({ company }) => ({ name: company.name, url: ["freelancer-space.company.show", [company.id]] }),
+    canAccess: user => user.role === "freelancer" || user.role === "admin" || user.role === "super-admin",
+    pageItems: ({ company }) => [{ name: "Details", url: ["freelancer-space.company.show", [company.id]], isActive: true }],
+  },
+} as { [key: string]: Page.PageDetailsBuilder<unknown> };
