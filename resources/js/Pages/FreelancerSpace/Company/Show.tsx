b71301a1ed6f7f7fe5b0b7a376f5f28464a3d16f
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-11-24 01:18:05 +0200

    feat: integrate ModelDetails component to display company information with links

--- resources/js/Pages/FreelancerSpace/Company/Show.tsx
+++ resources/js/Pages/FreelancerSpace/Company/Show.tsx
@@ -1,3 +1,4 @@
+import ModelDetails from "@/components/ModelDetails";
 import React from "react";
 
 type Props = {
@@ -5,7 +6,62 @@ type Props = {
 };
 
 function Show({ company }: Props) {
-  return <div>{JSON.stringify(company, null, 2)}</div>;
+  const companyDetails = [
+    {
+      label: "Default address",
+      value: company.address_1,
+      link: {
+        href: `https://www.google.com/maps/search/?api=1&query=${company.address_1}`,
+        isInner: false,
+        onBlank: true,
+      },
+    },
+    {
+      label: "Additional address",
+      value: company.address_2,
+      link: {
+        href: `https://www.google.com/maps/search/?api=1&query=${company.address_2}`,
+        isInner: false,
+        onBlank: true,
+      },
+    },
+    {
+      label: "Default phone",
+      value: company.phone_1_readable,
+      link: {
+        href: company.phone_1_rfc3966,
+        isInner: false,
+        onBlank: false,
+      },
+    },
+    {
+      label: "Additional phone",
+      value: company.phone_2_readable,
+      link: {
+        href: company.phone_2_rfc3966,
+        isInner: false,
+        onBlank: false,
+      },
+    },
+    {
+      label: "State",
+      value: company.state,
+    },
+    {
+      label: "Zip code",
+      value: company.zip_code,
+    },
+    {
+      label: "Preferred payment method",
+      value: company.preferred_payment_method_readable,
+    },
+  ];
+
+  return (
+    <div className="">
+      <ModelDetails items={companyDetails} />
+    </div>
+  );
 }
 
 export default Show;
