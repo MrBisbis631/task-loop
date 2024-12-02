eca83cbb1f404793c444c2ec177cec385a1ffb81
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-02 20:32:06 +0200

    feat: add googleMapsLink utility function for generating Google Maps URLs

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function googleMapsLink(address: string) {
+  return `https://www.google.com/maps/search/?api=1&query=${address}`
+}
