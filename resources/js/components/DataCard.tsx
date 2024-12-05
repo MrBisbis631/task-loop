f60385d959a8739d7cc182d93cfe9c8f8e903d22
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-06 01:28:46 +0200

    fix: add `flex-auto` and justify actions to end

--- resources/js/components/DataCard.tsx
+++ resources/js/components/DataCard.tsx
@@ -34,59 +34,61 @@ export default function DataCard({ description, title, items, actions }: CardDat
   };
 
   return (
-    <Card className="max-w-md">
-      {/* card title & description */}
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{description}</CardDescription>
-      </CardHeader>
+    <Card className="max-w-md flex-auto flex flex-col justify-between">
+      <div className="">
+        {/* card title & description */}
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
 
-      {/* card content with copy action */}
-      <CardContent className="grid grid-cols-[max-content_1fr_max-content_1fr] gap-x-1 gap-y-0.5">
-        {items
-          .filter(item => item.value)
-          .map(({ label, value, link }, index) => (
-            <>
-              <Button key={`${id}-${index}-title`} type="button" variant={"ghost"} size={"icon"} className="size-6" onClick={() => copyAction(value)}>
-                <ClipboardIcon width={"14"} />
-              </Button>
+        {/* card content with copy action */}
+        <CardContent className="grid grid-cols-[max-content_1fr_max-content_1fr] gap-x-1 gap-y-0.5">
+          {items
+            .filter(item => item.value)
+            .map(({ label, value, link }, index) => (
+              <>
+                <Button key={`${id}-${index}-title`} type="button" variant={"ghost"} size={"icon"} className="size-6" onClick={() => copyAction(value)}>
+                  <ClipboardIcon width={"14"} />
+                </Button>
 
-              <div key={`${id}-${index}-value`} className="">
-                {/* label with link icon if link exists */}
-                <span className="font-semibold text-sm relative">
-                  {label}
-                  {link ? <Link1Icon width={12} className="absolute -top-1 right-0 translate-x-full rotate-45" /> : null}
-                </span>
+                <div key={`${id}-${index}-value`} className="">
+                  {/* label with link icon if link exists */}
+                  <span className="font-semibold text-sm relative">
+                    {label}
+                    {link ? <Link1Icon width={12} className="absolute -top-1 right-0 translate-x-full rotate-45" /> : null}
+                  </span>
 
-                {/* tooltip with value & link if there is */}
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <div className="max-w-40 text-sm flex gap-1 items-center">
-                        {link ? (
-                          link.isInner ? (
-                            <Link href={link.href} className="hover:underline focus:underline truncate" target={link.onBlank ? "_blank" : ""}>
-                              {value}
-                            </Link>
+                  {/* tooltip with value & link if there is */}
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <div className="max-w-40 text-sm flex gap-1 items-center">
+                          {link ? (
+                            link.isInner ? (
+                              <Link href={link.href} className="hover:underline focus:underline truncate" target={link.onBlank ? "_blank" : ""}>
+                                {value}
+                              </Link>
+                            ) : (
+                              <a href={link.href} className="hover:underline focus:underline truncate" target={link.onBlank ? "_blank" : ""}>
+                                {value}
+                              </a>
+                            )
                           ) : (
-                            <a href={link.href} className="hover:underline focus:underline truncate" target={link.onBlank ? "_blank" : ""}>
-                              {value}
-                            </a>
-                          )
-                        ) : (
-                          <span className="truncate">{value}</span>
-                        )}
-                      </div>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <span>{value}</span>
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              </div>
-            </>
-          ))}
-      </CardContent>
+                            <span className="truncate">{value}</span>
+                          )}
+                        </div>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <span>{value}</span>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </div>
+              </>
+            ))}
+        </CardContent>
+      </div>
 
       {/* actions */}
       {actions ? <CardFooter>{actions}</CardFooter> : null}
