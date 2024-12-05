ab3661d52e074704853ad9fbdb7e98c71064708f
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-05 22:46:26 +0200

    fix: add nullable, require_with rules and activity_status enum fields

--- app/Http/Requests/UpdateCompanyRequest.php
+++ app/Http/Requests/UpdateCompanyRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Enums\CompanyActivityStatusEnum;
 use App\Enums\CompanyTypeEnum;
 use App\Enums\PaymentMethodEnum;
 use App\Enums\PaymentTermEnum;
@@ -28,29 +29,30 @@ class UpdateCompanyRequest extends FormRequest
     public function rules(): array
     {
         return [
-                'name' => 'string|required|sometimes',
-                'phone_1' => 'string|sometimes|phone',
-                'phone_2' => 'string|required_with:phone_1|phone',
-                'company_type' => ['sometimes', Rule::enum(CompanyTypeEnum::class)],
-                'website_url' => 'string|active_url|sometimes',
-                'linkedin_url' => 'string|url:linkedin|sometimes',
-                'facebook_url' => 'string|url:facebook|sometimes',
-                'instagram_url' => 'string|url:instagram|sometimes',
-                'country' => ['sometimes', Rule::enum(CountryAlpha2::class), 'required_with:state,zip_code,address_1,address_2'],
-                'state' => 'sometimes|string',
-                'zip_code' => 'sometimes|string',
-                'address_1' => 'sometimes|string',
-                'address_2' => 'sometimes|string|required_with:address_1',
-                'tax_identification_number' => 'sometimes|string',
-                'vat_number' => 'sometimes|string',
-                'tax_region_country' => 'sometimes|string',
-                'tax_filing_category' => 'sometimes|string',
-                'tax_documentation_url' => 'sometimes|string',
-                'preferred_payment_method' => ['sometimes', Rule::enum(PaymentMethodEnum::class)],
-                'bank_account_details' => 'sometimes|string',
-                'billing_address' => 'sometimes|string',
-                'payment_terms' => ['sometimes', Rule::enum(PaymentTermEnum::class)],
-                'preferred_currency' => ['sometimes', Rule::enum(CurrencyAlpha3::class)],
+            'name' => 'string|sometimes',
+            'phone_1' => 'string|sometimes|phone',
+            'phone_2' => 'string|required_with:phone_1|phone|nullable',
+            'company_type' => ['sometimes', Rule::enum(CompanyTypeEnum::class)],
+            'website_url' => 'string|active_url|sometimes|nullable',
+            'linkedin_url' => 'string|url:linkedin|sometimes|nullable',
+            'facebook_url' => 'string|url:facebook|sometimes|nullable',
+            'instagram_url' => 'string|url:instagram|sometimes|nullable',
+            'country' => ['sometimes', Rule::enum(CountryAlpha2::class), 'required_with:state,zip_code,address_1,address_2'],
+            'state' => 'sometimes|string|required_with:country,zip_code,address_1,address_2',
+            'zip_code' => 'sometimes|string|required_with:country,state,address_1,address_2',
+            'address_1' => 'sometimes|string|required_with:country,state,zip_code',
+            'address_2' => 'sometimes|string|required_with:address_1',
+            'tax_identification_number' => 'sometimes|string|nullable',
+            'vat_number' => 'sometimes|string|nullable',
+            'tax_region_country' => 'sometimes|string|nullable',
+            'tax_filing_category' => 'sometimes|string|nullable',
+            'tax_documentation_url' => 'sometimes|string|nullable',
+            'preferred_payment_method' => ['sometimes', Rule::enum(PaymentMethodEnum::class)],
+            'bank_account_details' => 'sometimes|string|nullable',
+            'billing_address' => 'sometimes|string',
+            'payment_terms' => ['sometimes', Rule::enum(PaymentTermEnum::class)],
+            'preferred_currency' => ['sometimes', Rule::enum(CurrencyAlpha3::class)],
+            'activity_status' => ['sometimes', Rule::enum(CompanyActivityStatusEnum::class)],
         ];
     }
 }
