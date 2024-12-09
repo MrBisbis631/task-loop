427254eee1d58d5089ae96dc1d573f1888fd3c96
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-09 23:21:04 +0200

    feat: enhance CompanyContactController index method with query and activity status filtering

--- app/Http/Controllers/CompanyContactController.php
+++ app/Http/Controllers/CompanyContactController.php
@@ -2,21 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\CompanyContactActivityStatusEnum;
+use App\Enums\ContactMethodEnum;
 use App\Http\Requests\StoreCompanyContactRequest;
 use App\Http\Requests\UpdateCompanyContactRequest;
+use App\Http\Resources\CompanyContactResource;
+use App\Http\Resources\CompanyResource;
+use App\Models\Company;
 use App\Models\CompanyContact;
 use App\Models\User;
 use Illuminate\Container\Attributes\CurrentUser;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Inertia\Inertia;
 
 class CompanyContactController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Company $company, Request $request)
     {
-        //
+        $query = $request->get("query", "");
+        $activityStatus = $request->input('activityStatus', "");
+
+        return Inertia::render("FreelancerSpace/Company/CompanyContact/Index", [
+            "query" => $query ?? null,
+            "activityStatus" => $activityStatus ?? null,
+
+            "contactMethodEnumAsArray" => ContactMethodEnum::toArray(),
+            "companyContactActivityStatusEnumAsArray" => CompanyContactActivityStatusEnum::toArray(),
+
+            "company" => CompanyResource::make($company),
+            "companyContacts" => CompanyContactResource::collection(
+                $company->companyContacts()
+                    ->when($query != "", function($q) use ($query) {
+                        $q->where("first_name", "like", "$query%")
+                            ->orWhere("last_name", "like", "$query%")
+                            ->orWhere("email", "like", "$query%");
+
+                        return $q;
+                    })
+                    ->when($activityStatus != "", fn($query) => $query->where("activity_status", $activityStatus))
+                    ->paginate(10)
+                    ->withQueryString(),
+            ),
+        ]);
     }
 
     /**
