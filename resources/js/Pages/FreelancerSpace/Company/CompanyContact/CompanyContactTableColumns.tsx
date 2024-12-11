5b056e27e9ba5891bd02d9cbb638424b73cf1673
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-12 00:26:12 +0200

    feat: add CompanyContactTableColumns and integrate with Index component for displaying company contacts

---
+++ resources/js/Pages/FreelancerSpace/Company/CompanyContact/CompanyContactTableColumns.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import { EmailCell, PhoneCell, TextCell } from "@/components/Cell";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+
+import { MoreVertical } from "lucide-react";
+import { Button } from "@/components/ui/button";
+export const columns: ColumnDef<App.Http.Resources.CompanyContactResource>[] = [
+  {
+    id: "name",
+    accessorKey: "name",
+    header: () => "Name",
+    accessorFn: col => `${col.first_name} ${col.last_name}`,
+    cell: ({ row }) => <TextCell>{`${row.original.first_name} ${row.original.last_name}`}</TextCell>,
+  },
+  {
+    id: "job_title",
+    accessorKey: "job_title",
+    header: () => "Job Title",
+    cell: ({ row }) => <TextCell>{row.original.job_title}</TextCell>,
+  },
+  {
+    id: "email",
+    accessorKey: "email",
+    header: () => "Email",
+    cell: ({ row }) => <EmailCell email={row.original.email} />,
+  },
+  {
+    id: "phone",
+    accessorKey: "phone",
+    header: () => "Phone",
+    cell: ({ row }) => <PhoneCell phone_rfc3966={row.original.phone_rfc3966}>{row.original.phone_readable}</PhoneCell>,
+  },
+  {
+    id: "tags",
+    accessorKey: "tags",
+    header: () => "Tags",
+    cell: ({ row }) => <Tags companyContact={row.original} />,
+  },
+  {
+    id: "actions",
+    accessorKey: "actions",
+    header: () => null,
+    cell: ({ row }) => <Actions companyContact={row.original} />,
+  },
+];
+
+function Tags({ companyContact }: { companyContact: App.Http.Resources.CompanyContactResource }) {
+  return (
+    <div className="gap-1 flex flex-wrap">
+      {companyContact.activity_status_readable && <Badge className={cn(companyContact.activity_status === "active" ? "bg-green-500" : companyContact.activity_status === "inactive" ? "bg-red-500" : companyContact.activity_status === "in_validation" ? "bg-yellow-500" : null)}>{companyContact.activity_status_readable}</Badge>}
+      {companyContact.preferred_contact_method_readable && <Badge variant={"outline"}>{companyContact.preferred_contact_method_readable}</Badge>}
+    </div>
+  );
+}
+
+function Actions({ companyContact }: { companyContact: App.Http.Resources.CompanyContactResource }) {
+  return (
+    <div className="size-6 ml-auto">
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button variant={"ghost"} size={"icon"} className="size-6 my-auto">
+            <MoreVertical width={14} />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent side="left" align="start">
+          {/* TODO actions */}
+          Place content for the popover here.
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
--- resources/js/Pages/FreelancerSpace/Company/CompanyContact/Index.tsx
+++ resources/js/Pages/FreelancerSpace/Company/CompanyContact/Index.tsx
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { columns } from "./CompanyContactTableColumns";
+import ResourceCollectionTable from "@/components/ResourceCollectionTable";
+import { useRoute } from "ziggy-js";
+import { useDebounceValue } from "usehooks-ts";
+import { Input } from "@/components/ui/input";
+import { router } from "@inertiajs/react";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
 type Props = {
   query?: string;
@@ -12,11 +19,34 @@ type Props = {
     label: string;
   }[];
   company: App.Http.Resources.CompanyResource;
-  contacts: App.ResourceCollection<App.Http.Resources.CompanyContactResource>;
+  companyContacts: App.ResourceCollection<App.Http.Resources.CompanyContactResource>;
 };
 
-function Index({ companyContactActivityStatusEnumAsArray, contactMethodEnumAsArray, company, contacts, query, activityStatus }: Props) {
-  return <div>Index</div>;
+function Index({ companyContactActivityStatusEnumAsArray, contactMethodEnumAsArray, company, companyContacts, query, activityStatus }: Props) {
+  const route = useRoute();
+  const [search, setSearch] = useState(query ?? "");
+  const [debouncedSearch, _] = useDebounceValue(search, 200);
+
+  useEffect(() => {
+    if (search !== query) {
+      router.visit(route("freelancer-space.company.company-contact.index", [company.id]), {
+        method: "get",
+        only: ["companyContacts", "query"],
+        preserveState: true,
+        replace: true,
+        data: { query: search, page: 1 },
+      });
+    }
+  }, [debouncedSearch]);
+  return (
+    <div>
+      <label className="block mb-2 relative md:max-w-sm group">
+        <MagnifyingGlassIcon className="absolute size-4 left-2 top-1/2 -translate-y-1/2 transform text-muted-foreground group-has-[:focus-visible]:text-slate-700" />
+        <Input type="search" placeholder="Search" className="pl-7" value={search} onChange={e => setSearch(e.target.value)} />
+      </label>{" "}
+      <ResourceCollectionTable {...companyContacts} columns={columns} resourceName="companyContacts" />
+    </div>
+  );
 }
 
 export default Index;
