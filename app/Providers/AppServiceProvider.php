9ac947a68c4314a59fa372a0c61a933744557ffd
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-12 14:11:48 +0200

    feat: implement Typesense client singleton and define search-engine feature flag

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,12 @@
 namespace App\Providers;
 
 use App\Enums\RoleEnum;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Pennant\Feature;
+use Typesense\Client;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // singleton for typesense client connection
+        $this->app->singleton(Client::class, fn() => (new Client(config('scout.typesense.client-settings'))));
     }
 
     /**
@@ -25,5 +30,9 @@ class AppServiceProvider extends ServiceProvider
         Gate::before(
             fn($user, $ability) => $user->role === RoleEnum::SUPER_ADMIN->value ? true : null
         );
+
+
+        // active `search-engine` flag if search engine is enabled
+        Feature::define('search-engine', fn() => Arr::get(app(Client::class)->health->retrieve(), 'ok', false));
     }
 }
