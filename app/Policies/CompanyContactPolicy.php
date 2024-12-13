ce390d456551ac8c9d3afe812ae1a6632f16d83b
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-13 02:24:43 +0200

    [BE]: fix `CompanyContactPolicy` to match `\Znck\Eloquent\Traits\BelongsToThrough` api

--- app/Policies/CompanyContactPolicy.php
+++ app/Policies/CompanyContactPolicy.php
@@ -3,23 +3,71 @@
 namespace App\Policies;
 
 use App\Enums\RoleEnum;
-use App\Models\Company;
+use App\Models\CompanyContact;
 use App\Models\User;
-use App\Traits\HasCommonUserPolicy;
+use Illuminate\Auth\Access\Response;
 
 class CompanyContactPolicy
 {
-    use HasCommonUserPolicy;
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, CompanyContact $companyContact): Response
+    {
+        return $companyContact?->user->id === $user->id ?
+            Response::allow() :
+            Response::denyAsNotFound();
+    }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user, Company $company): bool
+    public function create(User $user): bool
     {
-        return $company->user()->is($user)
-            && $user->hasRole([
-                RoleEnum::ADMIN,
-                RoleEnum::FREELANCER,
-            ]);
+        // only freelancers and admins can create company contacts
+        return (bool)$user && $user->hasRole([
+            RoleEnum::ADMIN,
+            RoleEnum::FREELANCER,
+        ]);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, CompanyContact $companyContact): bool
+    {
+        return $companyContact?->user->id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, CompanyContact $companyContact): bool
+    {
+        return $companyContact?->user->id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, CompanyContact $companyContact): bool
+    {
+        return $companyContact?->user->id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, CompanyContact $companyContact): bool
+    {
+        return $companyContact?->user->id === $user->id;
     }
 }
