705b82dba205f64ebe1f322f08e1e1f0c8fa24b6
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-13 02:26:14 +0200

    fix: update CompanyContactResource to include 'id' field and change activity status to 'in_vacation'

--- app/Http/Resources/CompanyContactResource.php
+++ app/Http/Resources/CompanyContactResource.php
@@ -11,6 +11,7 @@ use Propaganistas\LaravelPhone\PhoneNumber;
 use libphonenumber\PhoneNumberFormat;
 
 #[TypeScriptType([
+    'id' => 'number',
     'first_name' => 'string',
     'last_name' => 'string',
     'email' => 'string',
