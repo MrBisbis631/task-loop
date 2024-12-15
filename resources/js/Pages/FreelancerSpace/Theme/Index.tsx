e00fabb0b6f821146947e52f70897c63e24c618b
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-15 22:55:42 +0200

    add Index component for theme display in FreelancerSpace

---
+++ resources/js/Pages/FreelancerSpace/Theme/Index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+  theme: App.Http.Resources.ThemeResource;
+};
+
+function Index({ theme }: Props) {
+  return <div>Index</div>;
+}
+
+export default Index;
