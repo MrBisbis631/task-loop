9e7499d6aa66b5ffc125a2c3124b91c225fec94e
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-19 00:12:41 +0200

    feat: implement tabbed interface for theme customization with Colors & Typography and Custom CSS sections

--- resources/js/Pages/FreelancerSpace/Theme/Index.tsx
+++ resources/js/Pages/FreelancerSpace/Theme/Index.tsx
@@ -1,15 +1,52 @@
-import ColorPicker from "@/components/ui/color-picker";
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card, CardHeader, CardDescription, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
+import ThemeCustomCssForm from "./ThemeCustomCssForm";
+import ThemeColorTypographyForm from "./ThemeColorTypographyForm";
 
 type Props = {
   theme: App.Http.Resources.ThemeResource;
 };
 
-function Index({ theme }: Props) {
-  const [color, setColor] = React.useState("#fff");
-  return (<div>
-    <ColorPicker color={color} onSetColor={setColor} />
-  </div>);
-}
+export default function Index({ theme }: Props) {
+  return (
+    <div>
+      <Tabs defaultValue="colors-typography" className="flex flex-col max-w-xl">
+        <TabsList>
+          <TabsTrigger value="colors-typography" className="flex-1">
+            Colors & Typography
+          </TabsTrigger>
+          <TabsTrigger value="custom-css" className="flex-1">
+            Custom CSS
+          </TabsTrigger>
+        </TabsList>
 
-export default Index;
+        {/* colors */}
+        <TabsContent value="colors-typography">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Colors & Typography</CardTitle>
+              <CardDescription>Set your theme colors and typography</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ThemeColorTypographyForm theme={theme} />
+            </CardContent>
+          </Card>
+        </TabsContent>
+
+        {/* custom-css */}
+        <TabsContent value="custom-css">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-xl">Custom CSS</CardTitle>
+              <CardDescription>Set your custom CSS variables</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ThemeCustomCssForm theme={theme} />
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
