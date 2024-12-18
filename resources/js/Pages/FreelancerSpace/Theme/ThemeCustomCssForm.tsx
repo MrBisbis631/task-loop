63d7968b6646855eeb99f537c97be616a5229137
MrBisbis631 <shmuelbisberg.acc@gmail.com>
2024-12-19 00:12:35 +0200

    start: create ThemeCustomCssForm component for applying custom CSS themes

---
+++ resources/js/Pages/FreelancerSpace/Theme/ThemeCustomCssForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {
+  theme: App.Http.Resources.ThemeResource;
+};
+
+export default function ThemeCustomCssForm({ theme }: Props) {
+  return <div>ThemeCustomCssForm</div>;
+}
